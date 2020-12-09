fe612d10992f8feabc235e852b7ec08019babc68
likhithakonduru <55331635+likhithakonduru@users.noreply.github.com>
2020-12-09 22:27:27 +0530

    Add files via upload

---
+++ Gesture Controlled Bot Codes/reciever_l293d_trial/reciever_l293d_trial.ino
@@ -0,0 +1,78 @@
+// RECIEVER CODE
+
+//NOTE :- THIS CODE IS USED WHEN CONTROLLING THE ROBOT VIA THE BREADBORD REMOTE
+
+#include <VirtualWire.h>
+class motor{
+  public:
+  int in1,in2;
+  motor(int a,int b){
+    in1 = a; in2 =b;    
+  }
+  void drive(int a,int b){
+    analogWrite(in1,a);
+    analogWrite(in2,b);
+  }
+};
+
+motor moto1(3,7),moto2(4,5);
+
+void setup()
+{     
+     pinMode(7,OUTPUT);
+     pinMode(3,OUTPUT);
+     pinMode(4,OUTPUT);
+     pinMode(5,OUTPUT);
+      Serial.begin(38400);  // Debugging only
+      Serial.println("setup");
+      pinMode(13, OUTPUT);
+      // Initialise the IO and ISR
+      vw_setup(2000);  // Bits per sec
+      vw_set_rx_pin(2);    //Rx Data pin to Digital  Pin 2
+      vw_rx_start();       // Start the receiver PLL running
+}//close setup
+
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if(vw_get_message(buf, &buflen)) // Non-blocking
+    {
+       int i;
+       Serial.print("hello");
+        Serial.print(buf[0]);
+        digitalWrite(13, true); // Flash a light to show received good message
+        // Message with a good checksum received, dump it.
+        for (i = 0; i < buflen; i++)
+        {
+                  Serial.print(buf[i]);//print received command
+                if(buf[i] == '1')//if button 1 is pressed.... i.e.forward buton
+                {
+                   moto1.drive(255,0);moto2.drive(255,0);
+                   Serial.println(" = forward");
+                }
+                if(buf[i] == '2')//if button 2 is pressed.... i.e.back buton
+                {
+                  moto1.drive(0,255);moto2.drive(0,255); 
+                  Serial.println(" = backward");
+                }
+                if(buf[i] == '3')//if button 3 is pressed.... i.e.left buton
+                {
+                  moto1.drive(255,0);moto2.drive(0,0); 
+                  Serial.println(" = left");
+                }
+                if(buf[i] == '4')//if button 4 is pressed.... i.e.left buton
+                {
+                  moto1.drive(0,0);moto2.drive(255,0);
+                  Serial.println(" = right");
+                }
+                delay(1000);
+                
+         }
+            /* Serial.print(" ");
+          }
+          Serial.println("");*/
+          digitalWrite(13, false);
+   }//close if
+}//close loop
---
+++ voice controlled bot/Workshop_Voice_Controled_bot/Workshop_Voice_Controled_bot.ino
@@ -0,0 +1,96 @@
+// Add SoftwareSerial.h library  
+#include<>
+// Add String.h library to code
+#include<>
+// Create serial port and initialize using  to 2,3 port RX,TX
+
+// initialize a string to store data named "input"
+
+// initialize a char to store temporary data named "com"
+ 
+// create a class named motor with two output pins and function to drive motors
+class motor{
+  public:
+  // create two integers for outputs
+  
+  // create a constructor to initialise output pins  
+  motor(int a,int b){
+        // create two analogWrite 
+  }
+
+  // create a function to drive the motor 
+  void drive(int a,int b){
+  //  create a write function for both pins use pwm pins (11,10) (8,9)
+  
+  }
+};
+
+// create two obecjets of class motor named moto1 and moto2 use constructor
+// to pass pin numbers, the pwm pins will be helpful to change speed (3,5,6,9,10,11) 
+motor moto1(),moto2();
+
+void setup() {
+  // Begin Serial communication
+  
+  // Check if serial communication works by printing a string on serial monitor
+  
+  // Start bluetooth port by setting baud rate to 38400
+  
+  // check if the bluetooth communication works. print a string on bluetooth monitor
+  
+  // initialise 4 output pins to control the motors use pinMode function to initialise
+  
+}
+
+void loop() {
+  
+  if()  //Check if the bluetooth has recieved any string,using "object.available" function
+   {       
+    ="";     // initialise the string to empty string
+    while()  // Now use a condition to check availability if char is available at input 
+    {        
+       // use "object.read" to read char value from input and store it's value in char variable
+       
+       // print the newly aquired character to serial port to check
+       
+       // now concatenate the char to the string to make a complete string 
+       
+    }   
+    // Now print the string to serial port to check control signals from external bluetooth device.
+
+   // you can notice every word starts with "*" and ends with "#"
+   }
+  
+  else{ 
+    Serial.println(input);
+    // write condition to check if string is "*forward#"  
+    if(()=="*forward#")
+    //  Now we need to drive both motors in clockWise direction
+    { moto1.drive(255,0);moto2.drive(255,0);}
+    
+    // write condition to check if string is "*left#" 
+    else if(()=="")
+    //second case "left" : now we need to drive right motor in clockWise direction and turn off left motor     
+    { moto1.drive();moto2.drive();}
+    
+    // write condition to check if string is "*right#"
+    else if()
+    // case "right" : now we need to drive left motor in clockWise direction and turn off right motor
+    {;}
+    
+    // write condition for reverse motion
+    elseq if()
+    // backword motion will have both motors rotating in anticlockWise direction
+    {;}
+   
+   // lets move bot slowly in forward direction
+   else if()
+   // case "slow" : now we need to rotate both motors in forward directions but at low speed than normal
+   {;}
+
+    // last condition to check if string is stop
+   else if()
+    // case "stop" : now we need to stop both motors
+   {;}
+  }
+}
